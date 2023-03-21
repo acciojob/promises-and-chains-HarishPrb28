0ff1c0b82cea9d9b6ea8dc3f073c919eee2da075
Harish  Parab <50201375+HarishPrb28@users.noreply.github.com>
2023-03-21 16:46:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+const form = document.querySelector('#myForm');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const nameInput = document.querySelector('#name');
+  const ageInput = document.querySelector('#age');
+  
+  if (nameInput.value.trim() === '' || ageInput.value.trim() === '') {
+    alert('Inputs cannot be empty!');
+    return;
+  }
+  
+  const promise = new Promise((resolve, reject) => {
+    const age = parseInt(ageInput.value);
+    if (age >= 18) {
+      setTimeout(() => {
+        resolve(`Welcome, ${nameInput.value}. You can vote.`);
+      }, 4000);
+    } else {
+      reject(`Oh sorry ${nameInput.value}. You aren't old enough.`);
+    }
+  });
+  
+  promise.then((message) => {
+    alert(message);
+  }).catch((error) => {
+    alert(error);
+  });
+  
+  form.reset();
+});
